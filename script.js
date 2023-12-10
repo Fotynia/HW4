2738af829d6d9fbf93ab65dd15332d094e71ae81
Fotynia <114238811+Fotynia@users.noreply.github.com>
2023-12-10 21:22:37 +0200

    четверта домашка

---
+++ script.js
@@ -0,0 +1,36 @@
+
+//написати функцію reverseString(str), 
+//яка приймає на вхід рядок і
+// повертає його у зворотному порядку.
+
+// function reverseString(str) {
+//     return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+//   }
+//   reverseString('Samsung');
+
+
+//написати функцію isPalindrome(str), 
+//яка приймає на вхід рядок і 
+//перевіряє, чи є введений рядок паліндромом.
+
+// function isPalindrome(str) {
+//     return str === str.split("").reverse().join("");
+//     }
+//  console.log(isPalindrome("ого"));
+//  console.log(isPalindrome("шалаш"));
+//  console.log(isPalindrome("небо"));
+ 
+
+// написати функцію findGCD(a, b), 
+//яка приймає на вхід два числа і
+// повертає їхній НСД.
+
+// function findGCD(a, b) {
+//         if (b > 0) { 
+//            let k = a % b;
+//           return gcd(b, k); 
+//         } 
+//           return a;  
+//           console.log(a);
+//         };
+    
